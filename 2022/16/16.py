517c908871601b472fe5ee583b7a5322535f25e8
Clément Pinard <clement.pinard@xxii.fr>
2022-12-16 18:12:10 +0100

    day16

--- 2022/16/16.py
+++ 2022/16/16.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from tqdm import tqdm
+import functools
 
 with open("input.txt") as f:
     input_data = f.read().strip().split("\n")
@@ -10,10 +11,12 @@ for line in input_data:
     key = line[1]
     flow = int(line[4].split("=")[1][:-1])
     dest = [l.strip(",") for l in line[9:]]
-    graph[key] = {"flow": flow, "dest": {k : 1 for k in dest}}
+    graph[key] = {"flow": flow, "dest": {k: 1 for k in dest}}
 
 
 complete_graph = {}
+
+
 def bfs(key):
     queue = [key]
     explored = []
@@ -29,6 +32,7 @@ def bfs(key):
                 if k != key:
                     complete_graph[key]["dest"][j] = 1 + complete_graph[key]["dest"][k]
                 queue.append(j)
+
 
 for key in graph:
     bfs(key)
@@ -47,46 +51,55 @@ for k in original_keys[1:]:
 
 print(graph)
 
+
+@functools.lru_cache(maxsize=None)
 def get_score_Q1(root, visited, remaining):
     total_score = 0
     total_path = [root]
     distances = graph[root]["dest"]
     if remaining > 1:
-        scores = {p: (remaining-distance-1) * graph[p]["flow"] for p, distance in distances.items() if p not in visited}
+        scores = {
+            p: (remaining - distance - 1) * graph[p]["flow"]
+            for p, distance in distances.items()
+            if p not in visited
+        }
         for dest, score in scores.items():
-            candidate_score, path = get_score_Q1(dest, visited + [dest], remaining - distances[dest] - 1)
+            candidate_score, path = get_score_Q1(
+                dest, visited.union({dest}), remaining - distances[dest] - 1
+            )
             if total_score < candidate_score + score:
                 total_score = candidate_score + score
-                total_path = [root] + path
+                total_path = (root, *path)
     return total_score, total_path
 
-def get_score_Q2(root1, root2, visited, remaining1, remaining2):
+
+def get_score_Q2(root, visited, remaining):
     total_score = 0
-    swapped = False
-    if remaining1 < remaining2:
-        root1, root2 = root2, root1
-        remaining1, remaining2 = remaining2, remaining1
-        swapped = True
-    
-    distances = graph[root1]["dest"]
-    total_path = [[root1], [root2]]
-    if remaining1 > 1:
-        scores = {p: (remaining1-distance-1) * graph[p]["flow"] for p, distance in distances.items() if p not in visited}
-        if root1 == "AA":
+    total_path = [[], []]
+    distances = graph[root]["dest"]
+    if remaining > 1:
+        scores = {
+            p: (remaining - distance - 1) * graph[p]["flow"]
+            for p, distance in distances.items()
+            if p not in visited
+        }
+        if root == "AA":
             iterator = tqdm(scores.items(), total=len(scores))
         else:
             iterator = scores.items()
         for dest, score in iterator:
-            candidate_score, path = get_score_Q2(dest, root2, visited + [dest], remaining1 - distances[dest] - 1, remaining2)
+            candidate_score, paths = get_score_Q2(
+                dest, visited.union({dest}), remaining - distances[dest] - 1
+            )
             if total_score < candidate_score + score:
                 total_score = candidate_score + score
-                if swapped:
-                    total_path = [path[1], [root1] + path[0]]
-                else:
-                    total_path = [[root1] + path[0], path[1]]
-                
+                total_path = [[root] + paths[0], paths[1]]
+        elephant_score, elephant_path = get_score_Q1("AA", visited, 26)
+        if total_score < elephant_score:
+            total_score = elephant_score
+            total_path = [[root], elephant_path]
     return total_score, total_path
 
-print(get_score_Q1(first, [first], 30))
-print(get_score_Q2(first, first, [first], 26, 26))
 
+print(get_score_Q1("AA", frozenset(["AA"]), 30))
+print(get_score_Q2("AA", frozenset(["AA"]), 26))
