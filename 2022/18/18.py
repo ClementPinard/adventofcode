968b87cff0959145847f079c239d96e8ae7d3620
Clément Pinard <clement.pinard@xxii.fr>
2022-12-19 00:34:23 +0100

    day18

--- 2022/18/18.py
+++ 2022/18/18.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy.ndimage import binary_fill_holes
 
 input_data = pd.read_csv("input.txt", header=None)
+
 
 def get_faces(cubes_coords):
     faces = 0
@@ -10,8 +12,10 @@ def get_faces(cubes_coords):
             column = column[dimension].sort_values()
             faces += 2 + 2 * (column.diff() > 1).sum()
     return faces
+
+
 faces = get_faces(input_data)
-#Q1
+# Q1
 print(faces)
 
 h, w, d = input_data.max()
@@ -19,30 +23,9 @@ volume = np.zeros((h + 1, w + 1, d + 1))
 for _, (i, j, k) in input_data.iterrows():
     volume[i, j, k] = 1
 
+volume = binary_fill_holes(volume)
+filled_cubes = pd.DataFrame((volume).nonzero()).T
+faces = get_faces(filled_cubes)
 
-def dfs(i, j, k):
-    positions = [
-        [i + 1, j, k],
-        [i - 1, j, k],
-        [i, j - 1, k],
-        [i, j + 1, k],
-        [i, j, k - 1],
-        [i, j, k + 1],
-    ]
-    for i1, j1, k1 in positions:
-        if i1 < 0 or j1 < 0 or k1 < 0:
-            continue
-        if i1 > h or j1 > w or k1 > d:
-            continue
-        if volume[i1, j1, k1] == 0:
-            volume[i1, j1, k1] = 2
-            dfs(i1, j1, k1)
-
-import sys
-sys.setrecursionlimit(volume.size)
-dfs(0, 0, 0)
-negative_cubes = pd.DataFrame((volume == 0).nonzero()).T
-negative_faces = get_faces(negative_cubes)
-
-#Q2
-print(faces - negative_faces)
+# Q2
+print(faces)
